5aede908a8abbe54b78e85f9e63e631ac9dfec9a
tanjain <tanuj.jain@oracle.com>
2020-08-04 11:32:16 -0700

    Updates

---
+++ precache-manifest.daa5535b82159836d340faccb2ae6f8e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a33a81a37ab44b4782be5beb80c9ab20",
+    "url": "/photo-port/index.html"
+  },
+  {
+    "revision": "50d9bbd766edcd3e3bf3",
+    "url": "/photo-port/static/css/2.ceb79488.chunk.css"
+  },
+  {
+    "revision": "61419af1cf2c541e1e99",
+    "url": "/photo-port/static/css/main.5eae3207.chunk.css"
+  },
+  {
+    "revision": "50d9bbd766edcd3e3bf3",
+    "url": "/photo-port/static/js/2.b344c79d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/photo-port/static/js/2.b344c79d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61419af1cf2c541e1e99",
+    "url": "/photo-port/static/js/main.d4daad9b.chunk.js"
+  },
+  {
+    "revision": "aa0e969ea7cf66d801c8",
+    "url": "/photo-port/static/js/runtime-main.75070f25.js"
+  },
+  {
+    "revision": "658ed75127296e9c3cc344a910ad3286",
+    "url": "/photo-port/static/media/EventUniverse.658ed751.png"
+  },
+  {
+    "revision": "1dbb9ef600b41e7bd419869031646e04",
+    "url": "/photo-port/static/media/RunBuddy.1dbb9ef6.png"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/photo-port/static/media/TanujJain_DummyResume.d41d8cd9.pdf"
+  },
+  {
+    "revision": "5d6f1db3f43408208c7212df63ef646f",
+    "url": "/photo-port/static/media/coverimage.5d6f1db3.jpg"
+  }
+]);
